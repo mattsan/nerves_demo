a648b2ad15324253c3d5e159987b7a840d333b7f
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-06-16 12:08:28 +0900

    initial commit: beacon

---
+++ beacon/lib/beacon.ex
@@ -0,0 +1,18 @@
+defmodule Beacon do
+  @moduledoc """
+  Documentation for Beacon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Beacon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
